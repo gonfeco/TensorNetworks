4bb0a9c011cf28e06d2b1d4d46cf61545c53e222
gonfeco <gonfeco@gmail.com>
2023-10-27 14:11:20 +0200

    New Reduced Rho computation

--- parent_hamiltonian_mps.py
+++ parent_hamiltonian_mps.py
@@ -51,49 +51,40 @@ def get_null_projectors(array):
     h_null = v_null @ np.conj(v_null.T)
     return h_null
 
-def reduced_rho_mpo_old(mps, free_indices, contraction_indices):
+def reduced_rho_mpo(mps, free_indices, contraction_indices):
     logger.debug("mps: {}".format([a.shape for a in mps]))
     i = 0
     tensor_out = mps[i]
     # Starting Tensor for Denisty Matrix
-    
     tensor = mps[0]
-
-
-    
     if i in free_indices:
-        
         tensor_out = contract_indices(tensor_out, tensor_out.conj(), [], [])
         tensor_out = tensor_out.transpose(0, 3, 1, 4, 2, 5)
         reshape = [
-            tensor_out.shape[0] * tensor_out.shape[1], 
+            tensor_out.shape[0] * tensor_out.shape[1],
             tensor_out.shape[2], tensor_out.shape[3],
-            tensor_out.shape[4] * tensor_out.shape[5], 
+            tensor_out.shape[4] * tensor_out.shape[5],
         ]
         tensor_out = tensor_out.reshape(reshape)
-
     elif i in contraction_indices:
-        
         tensor_out = contract_indices(tensor_out, tensor_out.conj(), [1], [1])
         tensor_out = tensor_out.transpose(0, 2, 1, 3)
         reshape = [
-            tensor_out.shape[0] * tensor_out.shape[1], 
+            tensor_out.shape[0] * tensor_out.shape[1],
             tensor_out.shape[2] * tensor_out.shape[3]
         ]
-        tensor_out = tensor_out.reshape(reshape)        
-        
+        tensor_out = tensor_out.reshape(reshape)
     else:
         raise ValueError("Problem with site i: {}".format(i))
-    
     for i in range(1, len(mps)):
         tensor = mps[i]
         if i in free_indices:
             tensor = contract_indices(tensor, tensor.conj(), [], [])
             tensor = tensor.transpose(0, 3, 1, 4, 2, 5)
             reshape = [
-                tensor.shape[0] * tensor.shape[1], 
+                tensor.shape[0] * tensor.shape[1],
                 tensor.shape[2], tensor.shape[3],
-                tensor.shape[4] * tensor.shape[5], 
+                tensor.shape[4] * tensor.shape[5],
             ]
             tensor = tensor.reshape(reshape)
 
@@ -101,36 +92,29 @@ def reduced_rho_mpo_old(mps, free_indices, contraction_indices):
             tensor = contract_indices(tensor, tensor.conj(), [1], [1])
             tensor = tensor.transpose(0, 2, 1, 3)
             reshape = [
-                tensor.shape[0] * tensor.shape[1], 
+                tensor.shape[0] * tensor.shape[1],
                 tensor.shape[2] * tensor.shape[3]
             ]
-            tensor = tensor.reshape(reshape)    
-            
+            tensor = tensor.reshape(reshape)
         else:
-            raise ValueError("Problem with site i: {}".format(i))        
-        
+            raise ValueError("Problem with site i: {}".format(i))
         logger.debug("Shape tensor_out in: %s", tensor_out.shape)
         #logger.debug("Shape tensor: %s", tensor.shape)
         tensor_out = mpo_contraction(tensor_out, tensor)
         logger.debug("Shape tensor_out out: %s", tensor_out.shape)
-        
-        
     tensor_out = contract_indices_one_tensor(tensor_out, [(0, 3)])
-    
     return tensor_out
 
 def reduced_rho_mps_old(mps, free_indices, contraction_indices):
     i = 0
     tensor_out = mps[i]
     # Starting Tensor for Denisty Matrix
-    
     if i in free_indices:
         tensor_out = contract_indices(tensor_out, tensor_out.conj(), [], [])
     elif i in contraction_indices:
         tensor_out = contract_indices(tensor_out, tensor_out.conj(), [1], [1])
     else:
         raise ValueError("Problem with site i: {}".format(i))
-    
     for i in range(1, len(mps)):
         tensor = mps[i]
         if i in free_indices:
@@ -139,45 +123,45 @@ def reduced_rho_mps_old(mps, free_indices, contraction_indices):
             tensor = contract_indices(tensor, tensor.conj(), [1], [1])
         else:
             raise ValueError("Problem with site i: {}".format(i))
-
         tensor_out = density_matrix_mps_contracion(tensor_out, tensor)
         logger.debug("\t tensor_out.shape: %s", tensor_out.shape)
     tensor_out = contract_indices_one_tensor(tensor_out, [(0, 2), (3, 5)])
-    
-    return tensor_out   
+    return tensor_out
+
 def contraction_pl(tensor):
     tensor = contract_indices(tensor, tensor.conj(), [1], [1])
     tensor = tensor.transpose(0, 2, 1, 3)
     reshape = [
-        tensor.shape[0] * tensor.shape[1], 
+        tensor.shape[0] * tensor.shape[1],
         tensor.shape[2] * tensor.shape[3]
     ]
     tensor = tensor.reshape(reshape)
     return tensor
 
-def reduced_rho_mpo(mps, free_indices, contraction_indices):
+def reduced_rho_mpo_fulllist(mps, free_indices, contraction_indices):
     opa = list(product([0, 1], repeat=2 * len(free_indices)))
-    
     reduced_rho = np.zeros(
         (2**len(free_indices), 2**len(free_indices)), dtype="complex")
-    
-    l = []
+    logger.debug("opa: {}".format(len(opa)))
     for step in opa:
         tensor_out = mps[0]
         if 0 in free_indices:
             i = free_indices.index(0)
-            tensor_out = np.kron(tensor_out[:, step[2*i], :], tensor_out.conj()[:, step[2*i+1], :])
+            tensor_out = np.kron(
+                tensor_out[:, step[2*i], :],
+                tensor_out.conj()[:, step[2*i+1], :])
         else:
-
             tensor_out = contraction_pl(tensor_out)
         for i in range(1, len(mps)):
             tensor = mps[i]
             if i in free_indices:
                 j = free_indices.index(i)
-                tensor = np.kron(tensor[:, step[2*j], :], tensor.conj()[:, step[2*j+1], :])
+                tensor = np.kron(
+                    tensor[:, step[2*j], :], tensor.conj()[:, step[2*j+1], :])
             elif i in contraction_indices:
                 tensor = contraction_pl(tensor)
             tensor_out = tensor_out @ tensor
+        #logger.debug("Shape tensor_out out: %s", tensor_out.shape)
         row = [v for i, v in enumerate(step) if i%2==0]
         row_int = sum([v * 2 ** i for i, v in enumerate(row)])
         #print(row, row_int)
